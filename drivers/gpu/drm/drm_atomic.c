55205df45b549ec8379952282ffff319bc430d14
Sultan Alsawaf <sultan@kerneltoast.com>
2021-07-04 21:06:03 -0400

    drm/msm: Reduce latency while completing non-blocking commits
    In order to complete a commit, we must first wait for the previous
    commit to finish up. This is done by sleeping, during which time the CPU
    can enter a deep idle state and take a while to finish processing the
    commit after the wait is over. We can alleviate this by optimistically
    assuming that the kthread this commit worker is running on won't migrate
    to another CPU partway through. We only do this for the non-blocking
    case where the commit completion is done in an asynchronous worker
    because the generic DRM code already does this for atomic ioctls. Since
    non-blocking commits are the common case, preemptively kick the prime
    CPU out of idle when the atomic ioctl runs so that it'll be ready to
    process the DRM IRQ and display commit kthreads.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [@0ctobot: Adapt for 4.19]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -2713,3 +2713,29 @@ out:
 
 	return ret;
 }
+
+int drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
+			  struct drm_file *file_priv)
+{
+	/*
+	 * Optimistically assume the current task won't migrate to another CPU
+	 * and restrict the current CPU to shallow idle states so that it won't
+	 * take too long to finish running the ioctl whenever the ioctl runs a
+	 * command that sleeps, such as for an "atomic" commit. Apply this
+	 * restriction to the prime CPU as well in anticipation of it processing
+	 * the DRM IRQ and any other display commit work, so that it wakes up
+	 * now if it's in a deep idle state.
+	 */
+	struct pm_qos_request req = {
+		.type = PM_QOS_REQ_AFFINE_CORES,
+		.cpus_affine = ATOMIC_INIT(BIT(raw_smp_processor_id()) |
+					   *cpumask_bits(cpu_prime_mask))
+	};
+	int ret;
+
+	pm_qos_add_request(&req, PM_QOS_CPU_DMA_LATENCY, 100);
+	ret = __drm_mode_atomic_ioctl(dev, data, file_priv);
+	pm_qos_remove_request(&req);
+
+	return ret;
+}
