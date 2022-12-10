2cd388b02514e7e7190c2d023f002d640907fb0a
Blaster4385 <venkateshchaturvedi12@gmail.com>
2022-12-10 09:39:57 +0530

    oplus_touchscreen: Bring back s gesture

--- drivers/input/touchscreen/oplus_touchscreen/Samsung/S6SY771/sec_drivers_s6sy771.c
+++ drivers/input/touchscreen/oplus_touchscreen/Samsung/S6SY771/sec_drivers_s6sy771.c
@@ -1500,17 +1500,19 @@ static int sec_get_gesture_info(void *chip_data, struct gesture_info *gesture)
         gesture->Point_start.x = (coord[0] << 4) | ((coord[2] >> 4) & 0x0F);
         gesture->Point_start.y = (coord[1] << 4) | ((coord[2] >> 0) & 0x0F);
         break;
+
 	case GESTURE_S:
-        gesture->gesture_type  = SGESTRUE;
-        gesture->Point_start.x = (coord[0] << 4) | ((coord[2] >> 4) & 0x0F);
-        gesture->Point_start.y = (coord[1] << 4) | ((coord[2] >> 0) & 0x0F);
-        gesture->Point_1st.x   = (coord[3] << 4) | ((coord[5] >> 4) & 0x0F);
-        gesture->Point_1st.y   = (coord[4] << 4) | ((coord[5] >> 0) & 0x0F);
-        gesture->Point_2nd.x   = (coord[6] << 4) | ((coord[8] >> 4) & 0x0F);
-        gesture->Point_2nd.y   = (coord[7] << 4) | ((coord[8] >> 0) & 0x0F);
-        gesture->Point_end.x   = (coord[9] << 4) | ((coord[11] >> 4) & 0x0F);
-        gesture->Point_end.y   = (coord[10] << 4) | ((coord[11] >> 0) & 0x0F);
-        break;
+		gesture->gesture_type  = SGESTRUE;
+		gesture->Point_start.x = (coord[0] << 4) | ((coord[2] >> 4) & 0x0F);
+		gesture->Point_start.y = (coord[1] << 4) | ((coord[2] >> 0) & 0x0F);
+		gesture->Point_1st.x   = (coord[3] << 4) | ((coord[5] >> 4) & 0x0F);
+		gesture->Point_1st.y   = (coord[4] << 4) | ((coord[5] >> 0) & 0x0F);
+		gesture->Point_2nd.x   = (coord[6] << 4) | ((coord[8] >> 4) & 0x0F);
+		gesture->Point_2nd.y   = (coord[7] << 4) | ((coord[8] >> 0) & 0x0F);
+		gesture->Point_end.x   = (coord[9] << 4) | ((coord[11] >> 4) & 0x0F);
+		gesture->Point_end.y   = (coord[10] << 4) | ((coord[11] >> 0) & 0x0F);
+		break;
+
     default:
         gesture->gesture_type = UnkownGesture;
         break;
