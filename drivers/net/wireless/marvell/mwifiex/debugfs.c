31c018320c3178c716f5029017fe4cce91e8806e
AK-Papon <wantedpapon@gmail.com>
2023-11-26 10:50:55 +0600

    Revert "wifi: mwifiex: fix memory leak in mwifiex_histogram_read()"
    This reverts commit 20ac05a07114fcfc365ebccdccdb529c15a7cc07.

--- drivers/net/wireless/marvell/mwifiex/debugfs.c
+++ drivers/net/wireless/marvell/mwifiex/debugfs.c
@@ -265,11 +265,8 @@ mwifiex_histogram_read(struct file *file, char __user *ubuf,
 	if (!p)
 		return -ENOMEM;
 
-	if (!priv || !priv->hist_data) {
-		ret = -EFAULT;
-		goto free_and_exit;
-	}
-
+	if (!priv || !priv->hist_data)
+		return -EFAULT;
 	phist_data = priv->hist_data;
 
 	p += sprintf(p, "\n"
@@ -324,8 +321,6 @@ mwifiex_histogram_read(struct file *file, char __user *ubuf,
 	ret = simple_read_from_buffer(ubuf, count, ppos, (char *)page,
 				      (unsigned long)p - page);
 
-free_and_exit:
-	free_page(page);
 	return ret;
 }
 
