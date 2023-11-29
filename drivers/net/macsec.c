a5a62fac764b5cdb264b40d08cf84fe3595b63e7
Greg Kroah-Hartman <gregkh@google.com>
2023-11-29 12:39:35 +0000

    Revert "net: add DEV_STATS_READ() helper"
    This reverts commit 1d5224e02a1c0be7da369ffac531c392f262b162 which is
    commit 0b068c714ca9479d2783cc333fff5bc2d4a6d45c upstream.
    
    It breaks the Android ABI so it should be reverted for now.  If it needs
    to come back later, it can do so in an ABI-safe way.
    
    Bug: 161946584
    Change-Id: I0c46ebd971d3a4511b4a291abe44b5b79c5700fc
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/net/macsec.c
+++ drivers/net/macsec.c
@@ -2999,9 +2999,9 @@ static void macsec_get_stats64(struct net_device *dev,
 		s->tx_bytes   += tmp.tx_bytes;
 	}
 
-	s->rx_dropped = dev->stats.rx_dropped;
-	s->tx_dropped = dev->stats.tx_dropped;
-	s->rx_errors = dev->stats.rx_errors;
+	s->rx_dropped = atomic_long_read(&dev->stats.__rx_dropped);
+	s->tx_dropped = atomic_long_read(&dev->stats.__tx_dropped);
+	s->rx_errors = atomic_long_read(&dev->stats.__rx_errors);
 }
 
 static int macsec_get_iflink(const struct net_device *dev)
